db4e2b885e060b50db93037ffb33f3a5476954d7
shavix <davidpr1994@gmail.com>
2016-12-19 11:40:09 -0800

    test

---
+++ GymShare/DPRNewWorkoutTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DPRNewWorkoutTableViewCell.h
+//  GymShare
+//
+//  Created by David Richardson on 11/24/16.
+//  Copyright © 2016 David Richardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPRNewWorkoutTableViewCell : UITableViewCell
+
+@end
---
+++ GymShare/DPRNewWorkoutTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  DPRNewWorkoutTableViewCell.m
+//  GymShare
+//
+//  Created by David Richardson on 11/24/16.
+//  Copyright © 2016 David Richardson. All rights reserved.
+//
+
+#import "DPRNewWorkoutTableViewCell.h"
+
+@implementation DPRNewWorkoutTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
