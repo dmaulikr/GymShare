50f13bb83b3e423599790b56e3fd4035b864b69d
shavix <davidpr1994@gmail.com>
2016-12-19 12:42:27 -0800

    ttvc

---
+++ GymShare/TestTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.m
+//  GymShare
+//
+//  Created by David Richardson on 12/19/16.
+//  Copyright © 2016 David Richardson. All rights reserved.
+//
+
+#import "TestTableViewCell.h"
+
+@implementation TestTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
