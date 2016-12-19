6116337f6928868bc25746f2debb8964cde9f024
shavix <davidpr1994@gmail.com>
2016-12-19 12:20:55 -0800

    testtableviewcell

---
+++ GymShare/TestTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewCell.h
+//  GymShare
+//
+//  Created by David Richardson on 12/19/16.
+//  Copyright © 2016 David Richardson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewCell : UITableViewCell
+
+@end
